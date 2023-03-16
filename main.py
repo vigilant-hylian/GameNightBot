9c20712ed3e13b514eebbb1a78bb60f0a3db952b
Claire Winters <vigilant.hylian@gmail.com>
2023-03-16 22:16:14 +1100

    readjusted directories, set up venv, gitignore, and example dotenv

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import os
+import discord
 from datetime import *
 from datetime import timedelta
+from dotenv import load_dotenv
 
-now = datetime.now()
-game = datetime(2023, 3, 22, 14, 30)
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-print(now)
-print(game)
-print(now+timedelta(days=28))
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+client.run(TOKEN)
+
+# now = datetime.now()
+# game = datetime(2023, 3, 22, 14, 30)
+
+# print(now)
+# print(game)
+# print(now+timedelta(days=28))
