ef9cba5cdbcbb453ba70a7a8a76cc71dcfe64176
Claire Winters <vigilant.hylian@gmail.com>
2023-03-17 16:17:34 +1100

    Created a basic table structure for storing the games

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,6 @@
-CREATE TABLE IF NOT EXISTS `blacklist` (
-  `user_id` varchar(20) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS `warns` (
-  `id` int(11) NOT NULL,
-  `user_id` varchar(20) NOT NULL,
-  `server_id` varchar(20) NOT NULL,
-  `moderator_id` varchar(20) NOT NULL,
-  `reason` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE "games" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"game_name"	TEXT NOT NULL,
+	"start_timestamp"	NUMERIC NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)
